c17c9c4aeeddbfcf9e53ece83ea1fe596639b7c6
AniisahIssaq <aniisah.issaq@ada.ac.uk>
2024-10-18 16:37:09 +0000

    final draft

--- rps.js
+++ rps.js
@@ -1,34 +1,26 @@
 function rockPaperScissors(player1, player2) {
-  if (player1 == player2) {
-    return "draw"
-  } else if (player1 == 'rock' && player2 == 'scissors') {
-    return ('player1');
-  } else if (player1 == 'paper' && player2 == 'rock') {
-    return ('player1');
-  } else if (player1 == 'scissors' && player2 == 'paper') {
-    return ('player1');
-  } else if (player1 == 'lizard' && player2 == 'spock') {
-    return ('player1');
-  } else if (player1 == 'spock' && player2 == 'scissors') {
-    return ('player1');
-  } else if (player1 == 'rock' && player2 == 'lizard') {
-    return ('player1');
-  } else if (player1 == 'rock' && player2 == 'lizard') {
-    return ('player1');
-  } else if (player1 == 'paper' && player2 == 'spock') {
-    return ('player1');
-  } else if (player1 == 'scissors' && player2 == 'lizard') {
-    return ('player1');
-  } else if (player1 == 'lizard' && player2 == 'paper') {
-    return ('player1');
-  } else if (player1 == 'spock' && player2 == 'rock') {
-    return ('player1');
-  } else {
-    return ('player2');
+  const wins = {
+    // this displays what each 'character' beats; for example, 'rock' beats both 'scissors' and 'lizard'.
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    spock: ['scissors', 'rock'],
+    lizard: ['paper', 'spock'],
   }
-}                
+
+  // if both players return the same selection, they have drawn and the code prints "draw".
+  if (player1 === player2) return "draw";
+  return wins[player1].includes(player2) ? "player1" : "player2";
+  // if player1 wins, their name is returned, if untrue, player2 is returned. 
+}
+console.log(rockPaperScissors());
+
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
 }
+
+
+
+
